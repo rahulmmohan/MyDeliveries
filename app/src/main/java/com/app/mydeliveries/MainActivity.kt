45c6c9ca658d3defaee582617407cdb9287f44c1
Rahul Mohan <Rahul.Mohan@marlabs.com>
2019-03-25 16:08:10 +0530

    Initial commit

---
+++ app/src/main/java/com/app/mydeliveries/MainActivity.kt
@@ -0,0 +1,12 @@
+package com.app.mydeliveries
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
